817a73dae500e6ba763cc990c2590c5e020e2317
Philip ryström <prys45@hotmail.com>
2017-05-22 14:01:04 +0200

    Add project files.

---
+++ src/BethanysPieShop/Controllers/XxxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class XxxController : Controller
+    {
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
